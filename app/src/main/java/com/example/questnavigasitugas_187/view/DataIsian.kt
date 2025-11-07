5cf5a6251812f1bc381e5310e428b27d2d5a37a1
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-11-08 02:22:15 +0700

    menambahkan variabel, remember untuk menyimpan nilai state, mutableStateOf untuk Membuat state yang bisa berubah, dan by agar bisa mengakses nilai

--- app/src/main/java/com/example/questnavigasitugas_187/view/DataIsian.kt
+++ app/src/main/java/com/example/questnavigasitugas_187/view/DataIsian.kt
@@ -2,10 +2,20 @@ package com.example.questnavigasitugas_187.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DataIsian(
     onSumbitSuccess: () -> Unit,
     onBerandaBtnClick: () -> Unit
-)
+) {
+    var namaLengkap by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var selectedJenisKelamin by remember { mutableStateOf("Laki-laki") }
+    var selectedStatusKawin by remember { mutableStateOf("Lajang") }
+    var showDialog by remember { mutableStateOf(false) }
+}
