7635830ce2afcb0f2d45cf69321970d4edec1390
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-11-08 02:25:05 +0700

    menambahkan scaffold dan mengatur topbar

--- app/src/main/java/com/example/questnavigasitugas_187/view/DataIsian.kt
+++ app/src/main/java/com/example/questnavigasitugas_187/view/DataIsian.kt
@@ -1,6 +1,9 @@
 package com.example.questnavigasitugas_187.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,4 +21,8 @@ fun DataIsian(
     var selectedJenisKelamin by remember { mutableStateOf("Laki-laki") }
     var selectedStatusKawin by remember { mutableStateOf("Lajang") }
     var showDialog by remember { mutableStateOf(false) }
+
+    Scaffold(
+        topBar = { TopAppBar(title = { Text("Formulir Pendaftaran") }) }
+    )
 }
