2e8fedf441477aee58eaa55346a00333f826340d
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-11-08 02:48:15 +0700

    menambahkan data class Peserta untuk menyimpan data

---
+++ app/src/main/java/com/example/questnavigasitugas_187/view/ListPesertaScreen.kt
@@ -0,0 +1,8 @@
+package com.example.questnavigasitugas_187.view
+
+data class Peserta (
+    val namaLengkap: String,
+    val jenisKelamin: String,
+    val statusKawin: String,
+    val alamat: String
+)
