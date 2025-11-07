489c273ec65118a1cd04275fca2c60fbb823b320
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-11-08 01:32:09 +0700

    membuat function dataapp

--- app/src/main/java/com/example/questnavigasitugas_187/DataApp.kt
+++ app/src/main/java/com/example/questnavigasitugas_187/DataApp.kt
@@ -1,7 +1,15 @@
 package com.example.questnavigasitugas_187
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
 enum class Navigasi {
     Splash,
     Formulirku,
     List
 }
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+)
