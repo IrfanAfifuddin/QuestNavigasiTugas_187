0f59399776a5d6b20275924f31c5f2ac3888fe98
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-11-08 02:14:33 +0700

    menambahkan column untuk text selamat datang dan mengatur tata letak nya

--- app/src/main/java/com/example/questnavigasitugas_187/view/Halaman1.kt
+++ app/src/main/java/com/example/questnavigasitugas_187/view/Halaman1.kt
@@ -1,8 +1,44 @@
 package com.example.questnavigasitugas_187.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.R
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Halaman1(onMasukClick: () -> Unit) {
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Spacer(modifier = Modifier.height(50.dp))
+            Text(
+                text = "Selamat Datang United",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = Color(0xFF5A4C9B)
+            )
+        }
+    }
 }
