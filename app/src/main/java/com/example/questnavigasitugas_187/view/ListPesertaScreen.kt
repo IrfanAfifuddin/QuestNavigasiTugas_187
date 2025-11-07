bc6b8bc618cddfd0619a3c6afc2b00074ba14e2b
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-11-08 02:52:39 +0700

    menambahkan function ParticipantCard

--- app/src/main/java/com/example/questnavigasitugas_187/view/ListPesertaScreen.kt
+++ app/src/main/java/com/example/questnavigasitugas_187/view/ListPesertaScreen.kt
@@ -1,13 +1,18 @@
 package com.example.questnavigasitugas_187.view
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.R
 import androidx.compose.material3.Scaffold
@@ -55,6 +60,23 @@ fun ListPesertaScreen(
         ) {
             items(dataPeserta) { peserta ->
                 ParticipantCard(peserta = peserta)
+            }
+        }
+    }
+}
+
+@Composable
+fun ParticipantCard(peserta: Peserta) {
+    Card(modifier = Modifier.fillMaxWidth(), elevation = CardDefaults.cardElevation(2.dp)) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                DataRowItem(label = stringResource(R.string.nama_lengkap), value = peserta.namaLengkap, modifier = Modifier.weight(1f))
+                DataRowItem(label = stringResource(R.string.jenis_kelamin), value = peserta.jenisKelamin, modifier = Modifier.weight(1f))
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Row(modifier = Modifier.fillMaxWidth()) {
+                DataRowItem(label = stringResource(R.string.status_kawin), value = peserta.statusKawin, modifier = Modifier.weight(1f))
+                DataRowItem(label = stringResource(R.string.alamat), value = peserta.alamat, modifier = Modifier.weight(1f))
             }
         }
     }
